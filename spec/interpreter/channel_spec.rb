7d15b300192dd0bddad4dbcd1752fc11312cdaeb
Bill Tozier <bill@tmesis.workantileexchange.com>
2009-09-01 12:13:43 -0400

    Committing whatever changes I made.

--- spec/interpreter/channel_spec.rb
+++ spec/interpreter/channel_spec.rb
@@ -1,17 +1,4 @@
 require File.join(File.dirname(__FILE__), "/../spec_helper")
 
 describe "channel" do
-  it "should have a 'name' attribute set at initialization"
-  
-  it "should raise a TypeError when the name is set to a non-string"
-  
-  it "should have a 'source' attribute which defaults to an empty hash"
-  
-  it "should allow setting the 'source' attribute as an optional param"
-  
-  it "should raise a TypeError when the 'source' is not set to a hash"  
-  
-  it "should return the result associated with 'name' from @source when asked for #value"
-  
-  it "should raise a TypeError if the value is not a Literal object"
 end
--- spec/interpreter/code_spec.rb
+++ spec/interpreter/code_spec.rb
@@ -1,56 +1,54 @@
 require File.join(File.dirname(__FILE__), "/../spec_helper")
 
-describe "code object:" do  
-  describe "contents" do
-    describe "#split" do
-      it "should produce an empty list if the block contains nothing"
-      
-      it "should produce a list with one entry when the block contains an imperative line" 
-      
-      it "should produce a list of the top-level entries when it contains multiple items"
-    end
+describe "code fragments" do  
+  describe "#split" do
+    it "should produce an empty list if the block contains nothing"
     
-    describe "#points" do
-      it "should return the number of lines in the listing"
-    end
+    it "should produce a list with one entry when the block contains an imperative line" 
     
-    describe "nth_point" do
-      it "should return the root itself when called with n=1"
-      
-      it "should raise an ArgumentError when called with n not in [1,Code.points]"
-      
-      it "should return the entirety of the point specified when n>1, including sub-points"
-    end
-    
-    
-    describe "types" do
-      it "should return an empty list for block-only code"
-      
-      it "should return the set of the stack names in every literal"
-      
-      it "should return the set of the stack names in every ERC"
-      
-      it "should return the set of the stack names in every instruction"
-      
-      it "should return the set of the stack names in every channel's value"
-    end
-    
-    describe "leaf_count" do
-      it "should return a count of the non-block lines in the listing"
-    end
-    
-    describe "point_list" do
-      it "should return an array that includes one entry for each point (i.e., line)"
-    end
-    
-    describe "delete_point" do
-      it "should delete the entire subtree with the appropriate point number"
-    end
-    
-    describe "replace_point" do
-      it "should replace the entire subtree with the appropriate point number"
-    end
-    
-    
+    it "should produce a list of the top-level entries when it contains multiple items"
   end
+  
+  describe "#points" do
+    it "should return the number of lines in the listing"
+  end
+  
+  describe "nth_point" do
+    it "should return the root itself when called with n=1"
+    
+    it "should raise an ArgumentError when called with n not in [1,Code.points]"
+    
+    it "should return the entirety of the point specified when n>1, including sub-points"
+  end
+  
+  
+  describe "types" do
+    it "should return an empty list for block-only code"
+    
+    it "should return the set of the stack names in every literal"
+    
+    it "should return the set of the stack names in every ERC"
+    
+    it "should return the set of the stack names in every instruction"
+    
+    it "should return the set of the stack names in every channel's value"
+  end
+  
+  describe "leaf_count" do
+    it "should return a count of the non-block lines in the listing"
+  end
+  
+  describe "point_list" do
+    it "should return an array that includes one entry for each point (i.e., line)"
+  end
+  
+  describe "delete_point" do
+    it "should delete the entire subtree with the appropriate point number"
+  end
+  
+  describe "replace_point" do
+    it "should replace the entire subtree with the appropriate point number"
+  end
+  
+  
 end
