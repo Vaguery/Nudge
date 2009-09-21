b3161d6a889c2caa7b9708e062c7e2d74b56f648
Bill Tozier <bill@tmesis.local>
2009-09-21 08:28:48 -0400

    Shifted class definitions for Literals, Ercs, Instructions and Channels to code.rb, in preparation for refactoring classes as subsclasses of program point

--- lib/interpreter/code.rb
+++ lib/interpreter/code.rb
@@ -16,6 +16,43 @@ module Nudge
     def points
       return @listing.split(/\n/).length
     end
-    
   end
+  
+  
+  class Literal
+    attr_accessor :type, :value
+    def initialize(type,value)
+      @type = type
+      @value = value
+    end
+  end
+  
+  
+  class Erc
+    attr_accessor :type, :value
+    def initialize(type, value=nil)
+      @type = type
+      @value = value
+    end
+  end
+  
+  
+  class Instruction
+    attr_accessor :name, :requirements, :effects
+    def initialize(name, req={}, eff={})
+      @name = name
+      @requirements = req
+      @effects = eff
+    end
+  end
+  
+  
+  class Channel
+    attr_accessor :name
+    def initialize(name)
+      @name = name
+    end
+  end
+  
+  
 end
