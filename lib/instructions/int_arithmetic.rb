1540b5cdd7aa95111aafedc945a3db47309b0797
Bill Tozier <bill@tmesis.local>
2009-09-22 22:05:14 -0400

    Threw in IntMultiplyInstruction just to see if it would work

--- lib/instructions/int_arithmetic.rb
+++ lib/instructions/int_arithmetic.rb
@@ -26,3 +26,30 @@ class IntAddInstruction < Instruction
   end
   
 end
+
+class IntMultiplyInstruction < Instruction
+  include Singleton
+  
+  attr :result
+  
+  def preconditions?
+    needs :int, 2
+  end
+  
+  def go
+    self.preconditions?
+    
+    begin
+      arg1 = Stack.stacks[:int].pop
+      arg2 = Stack.stacks[:int].pop
+      @result = LiteralPoint.new("int", arg1.value * arg2.value)
+    end
+    
+    self.outcomes
+  end
+  
+  def outcomes
+    pushes :int, @result
+  end
+  
+end
--- spec/instructions/int_arithmetic_spec.rb
+++ spec/instructions/int_arithmetic_spec.rb
@@ -78,8 +78,85 @@ describe "IntAddInstruction" do
         pending
       end
     end
-    
-    
   end
-  
+end
+
+describe "IntMultiplyInstruction" do
+  it "should be a singleton" do
+    i1 = IntMultiplyInstruction.instance
+    i2 = IntMultiplyInstruction.instance
+    i1.should === i2
+  end
+
+  [:preconditions?, :go, :outcomes].each do |methodName|
+    before(:each) do
+      @i1 = IntMultiplyInstruction.instance
+    end
+    it "should respond to #{methodName}" do
+      @i1.should respond_to(methodName)
+    end 
+  end
+
+  describe "three phases of #going" do
+    before(:each) do
+      @i1 = IntMultiplyInstruction.instance
+      Stack.cleanup
+      @int1 = LiteralPoint.new("int", 6)
+    end
+
+    describe "#preconditions?" do
+      it "should check that there are at least 2 ints" do
+        2.times {Stack.push!(:int,@int1)}
+        Stack.stacks[:int].depth.should == 2
+        @i1.preconditions?.should == true
+      end
+
+      it "should raise an error if the preconditions aren't met" do
+        1.times {Stack.push!(:int,@int1)}
+        lambda{@i1.preconditions?}.should raise_error(Instruction::NotEnoughStackItems)
+      end
+
+      it "should successfully run #go only if all preconditions are met" do
+        Stack.push!(:int,@int1)
+        Stack.push!(:int,@int1)
+        @i1.should_receive(:outcomes)
+        @i1.go
+      end
+    end
+
+    describe "#go" do
+      it "should pop the arguments" do
+        2.times {Stack.push!(:int,@int1)}
+        Stack.stacks[:int].depth.should == 2
+        @i1.stub!(:outcomes) # and do nothing
+        @i1.go
+        Stack.stacks[:int].depth.should == 0
+      end
+
+      it "determine the result value" do
+        2.times {Stack.push!(:int,@int1)}
+        Stack.stacks[:int].depth.should == 2
+        @i1.stub!(:outcomes) # and do nothing
+        @i1.go
+        @i1.result.value.should == 36
+      end
+
+      it "should raise the right exceptions if a bad thing happens" do
+        pending
+      end
+
+    end
+
+    describe "#outcomes" do
+      it "should push! the result" do
+        2.times {Stack.push!(:int,@int1)}
+        @i1.go
+        Stack.stacks[:int].peek.value.should == 36
+      end
+
+      it "should raise the right exception if something bad happens" do
+        pending
+      end
+    end
+  end
 end
