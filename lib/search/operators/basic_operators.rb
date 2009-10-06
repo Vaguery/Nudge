236c5ff2dd30e48ec540a2ecbc50f0056fbd0510
Bill Tozier <bill@tmesis.local>
2009-10-06 17:45:36 -0400

    specced and implemented UniformBackboneCrossover operator, which mixes root program points of two parents

--- lib/search/operators/basic_operators.rb
+++ lib/search/operators/basic_operators.rb
@@ -32,4 +32,30 @@ module Nudge
   
   class NondominatedSubset < SearchOperator
   end
+  
+  class UniformBackboneCrossover < SearchOperator
+    def generate(crowd, howMany = crowd.length, prob = 0.5)
+      result = []
+      howMany.times do
+        where = rand(crowd.length)
+        mom = crowd[where]
+        dad = crowd[ (where+1) % crowd.length ]
+        xover = "block {\n"
+        (0..mom.program.contents.length-1).each do |point|
+          if rand() < prob
+            xover << mom.program.contents[point].tidy + "\n"
+          else
+            begin
+              xover << dad.program.contents[point].tidy + "\n"
+            rescue
+              xover << mom.program.contents[point].tidy + "\n"
+            end
+          end
+        end
+        xover << "}"
+        result << Individual.new(xover)
+      end
+      return result
+    end
+  end
 end
---
+++ spec/search/operators/uniformBackboneCrossover_spec.rb
@@ -0,0 +1,54 @@
+require File.join(File.dirname(__FILE__), "./../../spec_helper")
+include Nudge
+
+describe "uniformBackboneCrossover operator" do
+  before(:each) do
+    newDudes = []
+    @myXover = UniformBackboneCrossover.new
+    @myGuesser = RandomGuess.new
+  end
+  
+  it "should be a kind of SearchOperator" do
+    @myXover.should be_a_kind_of(SearchOperator)
+  end
+  
+  it "should produce a set of Individuals when it receives #generate" do
+    newDudes = @myXover.generate(@myGuesser.generate({:types => [IntType]},2))
+    newDudes.should be_a_kind_of(Array)
+    newDudes.each {|dude| dude.should be_a_kind_of(Individual)}
+  end
+  
+  it "should produce the same number of Individuals it gets as a default" do
+    newDudes = @myXover.generate(@myGuesser.generate({:types => [IntType]},17))
+    newDudes.length.should == 17
+  end
+  
+  it "should have an optional parameter that specifies the number of offspring to produce" do
+    newDudes = @myXover.generate(@myGuesser.generate({:types => [IntType]},2),5)
+    newDudes.length.should == 5
+  end
+  
+  it "should only include backbone points from one of the parents in the offsprings' genomes" do
+    rents = @myGuesser.generate({:types => [IntType]},2)
+    newDudes = @myXover.generate(rents,1)
+    newDudes.length.should == 1
+    allParentalPoints = rents[0].program.contents + rents[1].program.contents
+    allTidied = allParentalPoints.collect {|pt| pt.tidy}
+    newDudes[0].program.contents.each {|pt| allTidied.should include(pt.tidy)}
+  end
+  
+  it "should return an identical individual if given only one parent" do
+    rent = @myGuesser.generate({:types => [IntType]},1)
+    newDudes = @myXover.generate(rent,3)
+    newDudes.each {|kid| kid.program.tidy.should == rent[0].program.tidy}
+  end
+  
+  
+  it "should not affect the original parents set in any way" do
+    rent = @myGuesser.generate({:types => [IntType]},2)
+    originalMom = rent[0].object_id
+    newDudes = @myXover.generate(rent,1)
+    rent[0].object_id.should == originalMom
+  end
+  
+end
