b733c362101f13def8676fbd6e06bba3c69ed985
Bill Tozier <bill@tmesis.local>
2009-11-16 10:20:24 -0500

    refactored stack instructions to use a kind of mixin for generic behavior, without killing the Instruction#all_instructions

--- lib/instructions/stack_manipulation.rb
+++ lib/instructions/stack_manipulation.rb
@@ -1,9 +1,16 @@
-class IntPopInstruction < Instruction
+module PopInstruction
+  attr_reader :target_stack
+  
+  def initialize(context, target_stack)
+    @target_stack = target_stack
+    super(context)
+  end
+  
   def preconditions?
-    needs :int, 1
+    needs @target_stack, 1
   end
   def setup
-    @result = @context.stacks[:int].pop
+    @result = @context.stacks[@target_stack].pop
   end
   def derive
   end
@@ -11,192 +18,231 @@ class IntPopInstruction < Instruction
   end
 end
 
+
+class IntPopInstruction < Instruction
+  include PopInstruction
+  def initialize(context)
+    super(context, :int)
+  end
+end
 
 
 class FloatPopInstruction < Instruction
+  include PopInstruction
+  def initialize(context)
+    super(context, :float)
+  end
+end
+
+
+
+
+
+module SwapInstruction
+  attr_reader :target_stack
+  
+  def initialize(context, target_stack)
+    @target_stack = target_stack
+    super(context)
+  end
+  
   def preconditions?
-    needs :float, 1
+    needs @target_stack, 2
   end
   def setup
-    @result = @context.stacks[:float].pop
+    @result1 = @context.stacks[@target_stack].pop
+    @result2 = @context.stacks[@target_stack].pop
   end
   def derive
   end
   def cleanup
+    pushes @target_stack, @result1
+    pushes @target_stack, @result2
   end
 end
-
 
 
 class IntSwapInstruction < Instruction
-  def preconditions?
-    needs :int, 2
-  end
-  def setup
-    @result1 = @context.stacks[:int].pop
-    @result2 = @context.stacks[:int].pop
-  end
-  def derive
-  end
-  def cleanup
-    pushes :int, @result1
-    pushes :int, @result2
+  include SwapInstruction
+  def initialize(context)
+    super(context, :int)
   end
 end
-
 
 
 class FloatSwapInstruction < Instruction
-  def preconditions?
-    needs :float, 2
-  end
-  def setup
-    @result1 = @context.stacks[:float].pop
-    @result2 = @context.stacks[:float].pop
-  end
-  def derive
-  end
-  def cleanup
-    pushes :float, @result1
-    pushes :float, @result2
+  include SwapInstruction
+  def initialize(context)
+    super(context, :float)
   end
 end
 
+
+
+
+
+module DuplicateInstruction
+  attr_reader :target_stack
+  
+  def initialize(context, target_stack)
+    @target_stack = target_stack
+    super(context)
+  end
+  
+  def preconditions?
+    needs @target_stack, 1
+  end
+  def setup
+    @arg1 = @context.stacks[@target_stack].peek.value
+  end
+  def derive
+    @result = LiteralPoint.new(@target_stack.to_s,@arg1)
+  end
+  def cleanup
+    pushes @target_stack, @result
+  end
+end
 
 
 class IntDuplicateInstruction < Instruction
-  def preconditions?
-    needs :int, 1
-  end
-  def setup
-    @arg1 = @context.stacks[:int].peek.value
-  end
-  def derive
-    @result = LiteralPoint.new("int",@arg1)
-  end
-  def cleanup
-    pushes :int, @result
+  include DuplicateInstruction
+  def initialize(context)
+    super(context, :int)
   end
 end
-
 
 
 class FloatDuplicateInstruction < Instruction
-  def preconditions?
-    needs :float, 1
-  end
-  def setup
-    @arg1 = @context.stacks[:float].peek.value
-  end
-  def derive
-    @result = LiteralPoint.new("float",@arg1)
-  end
-  def cleanup
-    pushes :float, @result
+  include DuplicateInstruction
+  def initialize(context)
+    super(context, :float)
   end
 end
 
 
+
+
+
+module RotateInstruction
+  attr_reader :target_stack
+  
+  def initialize(context, target_stack)
+    @target_stack = target_stack
+    super(context)
+  end
+  
+  def preconditions?
+    needs @target_stack, 3
+  end
+  def setup
+    @arg3 = @context.stacks[@target_stack].pop
+    @arg2 = @context.stacks[@target_stack].pop
+    @arg1 = @context.stacks[@target_stack].pop
+  end
+  def derive
+  end
+  def cleanup
+    pushes @target_stack, @arg2
+    pushes @target_stack, @arg3
+    pushes @target_stack, @arg1
+  end
+end
 
 
 class IntRotateInstruction < Instruction
-  def preconditions?
-    needs :int, 3
-  end
-  def setup
-    @arg3 = @context.stacks[:int].pop
-    @arg2 = @context.stacks[:int].pop
-    @arg1 = @context.stacks[:int].pop
-  end
-  def derive
-  end
-  def cleanup
-    pushes :int, @arg2
-    pushes :int, @arg3
-    pushes :int, @arg1
+  include RotateInstruction
+  def initialize(context)
+    super(context, :int)
   end
 end
-
 
 
 class FloatRotateInstruction < Instruction
-  def preconditions?
-    needs :float, 3
-  end
-  def setup
-    @arg3 = @context.stacks[:float].pop
-    @arg2 = @context.stacks[:float].pop
-    @arg1 = @context.stacks[:float].pop
-  end
-  def derive
-  end
-  def cleanup
-    pushes :float, @arg2
-    pushes :float, @arg3
-    pushes :float, @arg1
+  include RotateInstruction
+  def initialize(context)
+    super(context, :float)
   end
 end
 
 
+
+
+
+module DepthInstruction
+  attr_reader :target_stack
+  
+  def initialize(context, target_stack)
+    @target_stack = target_stack
+    super(context)
+  end
+  
+  def preconditions?
+    @context.stacks[@target_stack].depth != nil
+  end
+  def setup
+  end
+  def derive
+    @result = LiteralPoint.new("int",@context.stacks[@target_stack].depth)
+  end
+  def cleanup
+    pushes :int, @result
+  end
+end
 
 
 class IntDepthInstruction < Instruction
-  def preconditions?
-    @context.stacks[:int].depth != nil
-  end
-  def setup
-  end
-  def derive
-    @result = LiteralPoint.new("int",@context.stacks[:int].depth)
-  end
-  def cleanup
-    pushes :int, @result
+  include DepthInstruction
+  def initialize(context)
+    super(context, :int)
   end
 end
-
 
 
 class FloatDepthInstruction < Instruction
+  include DepthInstruction
+  def initialize(context)
+    super(context, :float)
+  end
+end
+
+
+
+
+
+module FlushInstruction
+  attr_reader :target_stack
+  
+  def initialize(context, target_stack)
+    @target_stack = target_stack
+    super(context)
+  end
+  
   def preconditions?
-    @context.stacks[:float].depth != nil
+    @context.stacks[@target_stack].depth != nil
   end
   def setup
   end
   def derive
-    @result = LiteralPoint.new("int",@context.stacks[:float].depth)
   end
   def cleanup
-    pushes :int, @result
+    @context.stacks[@target_stack].clear
   end
 end
-
 
 
 
 class IntFlushInstruction < Instruction
-  def preconditions?
-    @context.stacks[:int].depth != nil
-  end
-  def setup
-  end
-  def derive
-  end
-  def cleanup
-    @context.stacks[:int].clear
+  include FlushInstruction
+  def initialize(context)
+    super(context, :int)
   end
 end
 
 
 
 class FloatFlushInstruction < Instruction
-  def preconditions?
-    @context.stacks[:float].depth != nil
-  end
-  def setup
-  end
-  def derive
-  end
-  def cleanup
-    @context.stacks[:float].clear
+  include FlushInstruction
+  def initialize(context)
+    super(context, :float)
   end
 end
