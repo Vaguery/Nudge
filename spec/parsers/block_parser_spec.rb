6bd44980382f0b729e3293e5737e4119d7faa33f
Bill Tozier <bill@tmesis.local>
2010-01-27 13:03:12 -0500

    Codeblock parser.

---
+++ spec/parsers/block_parser_spec.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), "./../spec_helper")
+load_grammar('codeblock')
+include Nudge
+
+describe NudgeCodeblockParser do
+  before(:each) do
+    @parser = NudgeCodeblockParser.new()
+  end
+  
+  it { should parse("value int (8)") }
+  it { should parse("do int_add") }
+  it { should parse("ref x88") }
+  
+  it { should parse("block {}") }
+  it { should parse("block {  }") }
+  it { should parse("block {\n }") }
+  it { should parse("block { block { block {block{block{}}}} }") }
+  it { should parse("block {do int_add}") }
+  it { should parse("block{do int_add}") }
+  it { should parse("block{do int_add\ndo int_add}") }
+  
+  
+  it { should_not parse("value int (1)\ndo int_add") }
+  it { should_not parse(" ") }
+  it { should_not parse("block") }
+  it { should_not parse("block ") }
+end
