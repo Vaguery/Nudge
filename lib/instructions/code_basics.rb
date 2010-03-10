396f952761676d9b1a46849dbc7a3babe66b3de4
Bill Tozier <bill@tmesis.local>
2010-03-09 19:02:04 -0500

    code_car instruction; somebody ought to rename these someday soon

--- lib/instructions/code_basics.rb
+++ lib/instructions/code_basics.rb
@@ -229,3 +229,26 @@ class CodeCdrInstruction < Instruction
     pushes :code, @result
   end
 end
+
+
+
+class CodeCarInstruction < Instruction
+  def preconditions?
+    needs :code, 1
+  end
+  def setup
+    @arg = @context.stacks[:code].pop.value
+  end
+  def derive
+    tree = NudgeProgram.new(@arg)
+    if tree.linked_code.kind_of?(CodeblockPoint) && (tree.points > 2)
+      new_tree = tree[1].contents[0]
+    else
+      new_tree = tree
+    end
+    @result = ValuePoint.new("code", new_tree.listing)
+  end
+  def cleanup
+    pushes :code, @result
+  end
+end
