fb16bad56b70d51551803fd437f0ee37aa07ef60
Bill Tozier <bill@tmesis.local>
2010-03-09 20:34:33 -0500

    code_member_q instruction

--- lib/instructions/code_basics.rb
+++ lib/instructions/code_basics.rb
@@ -292,3 +292,32 @@ class CodeInstructionsInstruction < Instruction
     pushes :code, @result
   end
 end
+
+
+
+class CodeMemberQInstruction < Instruction
+  def preconditions?
+    needs :code, 2
+  end
+  def setup
+    @arg2 = @context.stacks[:code].pop.value
+    @arg1 = @context.stacks[:code].pop.value
+  end
+  def derive
+    looking_for_this = NudgeProgram.new(@arg1)
+    tree = NudgeProgram.new(@arg2)
+    if tree.parses? && looking_for_this.parses?
+      if tree.linked_code.kind_of?(CodeblockPoint)
+        found = tree[1].contents.any? {|branch| branch.listing == looking_for_this.listing}
+      else
+        found = (tree.listing == looking_for_this.listing)
+      end
+    else
+      found = false
+    end
+    @result = ValuePoint.new("bool", found)
+  end
+  def cleanup
+    pushes :bool, @result
+  end
+end
