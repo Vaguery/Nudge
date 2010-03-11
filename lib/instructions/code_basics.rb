ad50d9f6ebda1c128fa7f7cd7b931c4843f0779e
Bill Tozier <bill@tmesis.local>
2010-03-11 18:42:26 -0500

    code_position instruction

--- lib/instructions/code_basics.rb
+++ lib/instructions/code_basics.rb
@@ -426,3 +426,27 @@ class CodeContainsQInstruction < Instruction
     pushes :bool, @result
   end
 end
+
+
+class CodePositionInstruction < Instruction
+  def preconditions?
+    needs :code, 2
+  end
+  def setup
+    @arg2 = @context.stacks[:code].pop.value
+    @arg1 = @context.stacks[:code].pop.value
+  end
+  def derive
+    looking_for_this = NudgeProgram.new(@arg1).linked_code
+    inside_this = NudgeProgram.new(@arg2).linked_code
+    if inside_this && looking_for_this
+      index = inside_this.find_index {|point| point.listing == looking_for_this.listing} || -2
+    else
+      index = -2
+    end
+    @result = ValuePoint.new("int", index+1)
+  end
+  def cleanup
+    pushes :int, @result
+  end
+end
