355b1939f63476fd0015a6a31af737864ed34c6a
Bill Tozier <bill@tmesis.local>
2010-03-11 18:21:16 -0500

    CodeConcatenateInstruction

--- lib/instructions/code_basics.rb
+++ lib/instructions/code_basics.rb
@@ -278,6 +278,62 @@ class CodeCarInstruction < Instruction
 end
 
 
+class CodeListInstruction < Instruction
+  def preconditions?
+    needs :code, 2
+  end
+  def setup
+    @arg2 = @context.stacks[:code].pop.value
+    @arg1 = @context.stacks[:code].pop.value
+  end
+  def derive
+    t1 = NudgeProgram.new(@arg1).linked_code
+    t2 = NudgeProgram.new(@arg2).linked_code
+    listed = CodeblockPoint.new([t1,t2]).listing
+    @result = ValuePoint.new("code", listed)
+  end
+  def cleanup
+    pushes :code, @result
+  end
+end
+
+
+
+class CodeConcatenateInstruction < Instruction
+  def preconditions?
+    needs :code, 2
+  end
+  def setup
+    @arg2 = @context.stacks[:code].pop.value
+    @arg1 = @context.stacks[:code].pop.value
+  end
+  def derive
+    t1 = NudgeProgram.new(@arg1)
+    t2 = NudgeProgram.new(@arg2)
+    if t1.parses? && t2.parses?
+      if t1.linked_code.kind_of?(CodeblockPoint)
+        if t2.linked_code.kind_of?(CodeblockPoint)
+          new_tree = t1[1].contents + t2[1].contents
+        else
+          new_tree = t1[1].contents << t2.linked_code
+        end
+      else
+        new_tree = [t1.linked_code, t2.linked_code]
+      end
+      listed = CodeblockPoint.new(new_tree).listing
+      @result = ValuePoint.new("code", listed)
+    else
+      @result = ValuePoint.new("code", "block {}")
+    end
+  end
+  def cleanup
+    pushes :code, @result
+  end
+end
+
+
+
+
 
 class CodeExecuteInstruction < Instruction
   def preconditions?
--- spec/instructions/code_basics_spec.rb
+++ spec/instructions/code_basics_spec.rb
@@ -1028,3 +1028,87 @@ describe CodeListInstruction do
     end
   end
 end
+
+
+describe CodeConcatenateInstruction do
+  before(:each) do
+    @context = Interpreter.new
+    @i1 = CodeConcatenateInstruction.new(@context)
+  end
+  
+  it "should have the right context" do
+    @i1.context.should == @context
+  end
+  
+  [:preconditions?, :setup, :derive, :cleanup].each do |methodName|
+    it "should respond to \##{methodName}" do
+      @i1.should respond_to(methodName)
+    end
+  end
+  
+  describe "\#go" do
+    before(:each) do
+      @context = Interpreter.new
+      @i1 = CodeConcatenateInstruction.new(@context)
+    end
+    
+    describe "\#preconditions?" do
+      it "should need at least one item on the :code stack" do
+        @context.clear_stacks
+        lambda{@i1.preconditions?}.should raise_error
+        @context.stacks[:code].push(ValuePoint.new("code", "ref b"))
+        @context.stacks[:code].push(ValuePoint.new("code", "ref a"))
+        lambda{@i1.preconditions?}.should_not raise_error
+      end
+    end
+    
+    describe "\#cleanup" do
+      it "should push a new concatenated block containing the two :code items' code, in order" do
+        @context.stacks[:code].push(ValuePoint.new("code", "block {do a do b}"))
+        @context.stacks[:code].push(ValuePoint.new("code", "block {do c}"))
+        @i1.go
+        @context.stacks[:code].peek.value.should == "block {\n  do a\n  do b\n  do c}"
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "block {block {do a}}"))
+        @context.stacks[:code].push(ValuePoint.new("code", "block {block {do b}}"))
+        @i1.go
+        @context.stacks[:code].peek.value.should ==
+          "block {\n  block {\n    do a}\n  block {\n    do b}}"
+      end
+      
+      it "should append #2 to #1, if 2 is an atom and 1 a block" do
+        @context.stacks[:code].push(ValuePoint.new("code", "block {do a do b}"))
+        @context.stacks[:code].push(ValuePoint.new("code", "do c"))
+        @i1.go
+        @context.stacks[:code].peek.value.should == "block {\n  do a\n  do b\n  do c}"
+      end
+      
+      it "should wrap #1 and #2 if #1 isn't a block" do
+        @context.stacks[:code].push(ValuePoint.new("code", "do a"))
+        @context.stacks[:code].push(ValuePoint.new("code", "do b"))
+        @i1.go
+        @context.stacks[:code].peek.value.should == "block {\n  do a\n  do b}"
+      end
+      
+      it "should work with footnotes correctly" do
+        @context.stacks[:code].push(ValuePoint.new("code", "value «a»\n«a» 1"))
+        @context.stacks[:code].push(ValuePoint.new("code", "value «b»\n«b» 2"))
+        @i1.go
+        @context.stacks[:code].peek.value.should ==
+          "block {\n  value «a»\n  value «b»} \n«a» 1\n«b» 2"
+      end
+      
+      it "should return an empty block if either arg is unparseable" do
+        @context.stacks[:code].push(ValuePoint.new("code", "i got nuthin"))
+        @context.stacks[:code].push(ValuePoint.new("code", "value «b»\n«b» 2"))
+        @i1.go
+        @context.stacks[:code].peek.value.should == "block {}"
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "value «b»\n«b» 2"))
+        @context.stacks[:code].push(ValuePoint.new("code", "no idea"))
+        @i1.go
+        @context.stacks[:code].peek.value.should == "block {}"
+      end
+    end
+  end
+end
