6043bf7edf43c61d837b9c64462aaa56801e9ea2
William Tozier <bill@Abomination.local>
2010-03-20 21:21:57 -0400

    Found a bug in CodeNthInstruction where it was dividing by zero

--- lib/instructions/code_basics.rb
+++ lib/instructions/code_basics.rb
@@ -199,6 +199,7 @@ class CodeNthInstruction < Instruction
     tree = NudgeProgram.new(@arg2)
     if tree.linked_code.kind_of?(CodeblockPoint)
       pts = tree[1].contents.length
+      raise (InstructionMethodError, "#{self.class} can't work on empty blocks") if pts < 1
       val = tree[1].contents[@arg1 % pts]
     else
       val = tree
