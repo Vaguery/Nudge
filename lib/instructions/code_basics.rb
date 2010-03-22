f1d0681fec0bf55d2c3a1307e13417835cf3039a
Bill Tozier <bill@tmesis.local>
2010-03-22 18:43:11 -0400

    code_discrepancy instruction

--- lib/instructions/code_basics.rb
+++ lib/instructions/code_basics.rb
@@ -591,3 +591,53 @@ class CodeDoTimesInstruction < Instruction
     end
   end
 end
+
+
+
+class CodeParsesQInstruction < Instruction
+  def preconditions?
+    needs :code, 1
+  end
+  def setup
+    @arg1 = @context.stacks[:code].pop.value
+  end
+  def derive
+    answer = NudgeProgram.new(@arg1).parses? ? true : false
+    @result = ValuePoint.new("bool", answer)
+  end
+  def cleanup
+    pushes :bool, @result
+  end
+end
+
+
+
+# ORIGINAL DESCRIPTION in Push3:
+# Pushes a measure of the discrepancy between the top two CODE stack items onto the INTEGER stack. This will be zero if the top two items are equivalent, and will be higher the 'more different' the items are from one another. The calculation is as follows:
+# 1. Construct a list of all of the unique items in both of the lists (where uniqueness is determined by equalp). Sub-lists and atoms all count as items.
+# 2. Initialize the result to zero.
+# 3. For each unique item increment the result by the difference between the number of occurrences of the item in the two pieces of code.
+# 4. Push the result.
+
+
+class CodeDiscrepancyInstruction < Instruction
+  def preconditions?
+    needs :code, 2
+  end
+  def setup
+    @arg2 = @context.stacks[:code].pop.value
+    @arg1 = @context.stacks[:code].pop.value
+  end
+  def derive
+     # collect string listings of every point
+    parts_of_1 = NudgeProgram.new(@arg1).linked_code.collect {|pt| pt.listing}
+    parts_of_2 = NudgeProgram.new(@arg2).linked_code.collect {|pt| pt.listing}
+    unique_parts = (parts_of_1.uniq + parts_of_2.uniq).reject {|i| i == ""}
+    summed_differences = unique_parts.inject(0) {|sum, uniq_string| sum +
+      (parts_of_2.count(uniq_string) - parts_of_1.count(uniq_string)).abs}
+    @result = ValuePoint.new("int", summed_differences)
+  end
+  def cleanup
+    pushes :int, @result
+  end
+end
--- spec/instructions/code_basics_spec.rb
+++ spec/instructions/code_basics_spec.rb
@@ -1594,3 +1594,166 @@ describe CodeDoTimesInstruction do
     end
   end
 end
+
+
+
+describe CodeParsesQInstruction do
+  before(:each) do
+    @context = Interpreter.new
+    @i1 = CodeParsesQInstruction.new(@context)
+  end
+  
+  it "should check its context is set" do
+    @i1.context.should == @context
+  end
+  
+  [:preconditions?, :setup, :derive, :cleanup].each do |methodName|
+    it "should respond to \##{methodName}" do
+      @i1.should respond_to(methodName)
+    end   
+  end
+  
+  describe "\#go" do
+    before(:each) do
+      @i1 = CodeParsesQInstruction.new(@context)
+      @context.reset
+    end
+    
+    describe "\#preconditions?" do
+      it "should check that there is at least one :code item" do
+        lambda{@i1.preconditions?}.should raise_error
+        @context.stacks[:code].push(ValuePoint.new("code", "foo"))
+        @i1.preconditions?.should == true
+      end
+    end
+    
+    describe "\#cleanup" do
+      before(:each) do
+        @context.reset
+      end
+      
+      it "should push a boolean 'false' if the top :code item can't be parsed" do
+        @context.stacks[:code].push(ValuePoint.new("code", "foo"))
+        @i1.go
+        @context.stacks[:bool].peek.value.should == false
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "block {"))
+        @i1.go
+        @context.stacks[:bool].peek.value.should == false
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "doint_add"))
+        @i1.go
+        @context.stacks[:bool].peek.value.should == false
+      end
+      
+      it "should push a boolean 'true' value if the top :code item can be parsed" do
+        @context.stacks[:code].push(ValuePoint.new("code", "block {}"))
+        @i1.go
+        @context.stacks[:bool].peek.value.should == true
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "ref x"))
+        @i1.go
+        @context.stacks[:bool].peek.value.should == true
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "value «foo»\n«foo» bar"))
+        @i1.go
+        @context.stacks[:bool].peek.value.should == true
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "do some_weird_stuff"))
+        @i1.go
+        @context.stacks[:bool].peek.value.should == true
+      end
+      
+      it "should push false if the top item has no code (is only footnotes)" do
+        @context.stacks[:code].push(ValuePoint.new("code", " \n«foo» bar\n«baz» qux"))
+        @i1.go
+        @context.stacks[:bool].peek.value.should == false
+      end
+    end
+  end
+end
+
+
+
+describe CodeDiscrepancyInstruction do
+  before(:each) do
+    @context = Interpreter.new
+    @i1 = CodeDiscrepancyInstruction.new(@context)
+  end
+  
+  it "should check its context is set" do
+    @i1.context.should == @context
+  end
+  
+  [:preconditions?, :setup, :derive, :cleanup].each do |methodName|
+    it "should respond to \##{methodName}" do
+      @i1.should respond_to(methodName)
+    end   
+  end
+  
+  describe "\#go" do
+    before(:each) do
+      @i1 = CodeDiscrepancyInstruction.new(@context)
+      @context.reset
+    end
+    
+    describe "\#preconditions?" do
+      it "should check that there is at least two :code items" do
+        lambda{@i1.preconditions?}.should raise_error
+        @context.stacks[:code].push(ValuePoint.new("code", "foo"))
+        @context.stacks[:code].push(ValuePoint.new("code", "foo"))
+        @i1.preconditions?.should == true
+      end
+    end
+    
+    describe "\#cleanup" do
+      before(:each) do
+        @context.reset
+      end
+      
+      it "should calculate the summed absolute difference in counts of all points in both programs" do
+        @context.stacks[:code].push(ValuePoint.new("code", "ref x"))
+        @context.stacks[:code].push(ValuePoint.new("code", "ref y"))
+        @i1.go
+        @context.stacks[:int].peek.value.should == 2
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "block {ref y}"))
+        @context.stacks[:code].push(ValuePoint.new("code", "ref y"))
+        @i1.go
+        @context.stacks[:int].peek.value.should == 1
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "do x"))
+        @context.stacks[:code].push(ValuePoint.new("code", "do x"))
+        @i1.go
+        @context.stacks[:int].peek.value.should == 0
+      end
+      
+      it "should sum occurrences of different items, not just unique parts" do
+        @context.stacks[:code].push(ValuePoint.new("code", "block {do x do x do x do x}"))
+        @context.stacks[:code].push(ValuePoint.new("code", "block {do y}"))
+        @i1.go
+        @context.stacks[:int].peek.value.should == 7
+      end
+      
+      it "should work as you would expect for NilPoint (unparseable) programs" do
+        @context.stacks[:code].push(ValuePoint.new("code", "some weird crap"))
+        @context.stacks[:code].push(ValuePoint.new("code", "nothing here either"))
+        @i1.go
+        @context.stacks[:int].peek.value.should == 0
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "do real_program_stuff"))
+        @context.stacks[:code].push(ValuePoint.new("code", "refx")) # unparseable
+        @i1.go
+        @context.stacks[:int].peek.value.should == 1
+      end
+      
+      it "should not depend on typographic differences" do
+        @context.stacks[:code].push(ValuePoint.new("code", "block {ref y value «int» \n«int» 91}"))
+        @context.stacks[:code].push(ValuePoint.new("code",
+          "block   {\n  ref  y \tvalue    «int» \n\n«int»  \t91}"))
+        @i1.go
+        @context.stacks[:int].peek.value.should == 0
+      end
+    end
+  end
+end
