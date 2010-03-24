a8b419e40d9e3b84982b34e5e7d16756d08254a9
Bill Tozier <bill@tmesis.local>
2010-03-23 20:47:09 -0400

    code_cons instruction

--- lib/instructions/code_basics.rb
+++ lib/instructions/code_basics.rb
@@ -405,6 +405,34 @@ class CodeConcatenateInstruction < Instruction
 end
 
 
+# This is a tricky one to translate from Push, which assumes a very Lisp-like list structure. Here we'll simply build a new block with the consed item first, and the original code second.
+
+class CodeConsInstruction < Instruction
+  def preconditions?
+    needs :code, 2
+  end
+  def setup
+    @arg2 = @context.stacks[:code].pop.value
+    @arg1 = @context.stacks[:code].pop.value
+  end
+  def derive
+    t1 = NudgeProgram.new(@arg1)
+    t2 = NudgeProgram.new(@arg2)
+    if t1.parses? && t2.parses?
+      consed_tree = t2.insert_point_before(1,t1.linked_code)
+    else
+      raise InstructionMethodError,
+        "#{self.class.to_nudgecode} cannot parse the arguments"
+    end
+    @result = ValuePoint.new("code", consed_tree.listing)
+  end
+  def cleanup
+    pushes :code, @result
+  end
+end
+
+
+
 
 
 
--- spec/instructions/code_basics_spec.rb
+++ spec/instructions/code_basics_spec.rb
@@ -1436,6 +1436,74 @@ end
 
 
 
+describe CodeConsInstruction do
+  before(:each) do
+    @context = Interpreter.new
+    @i1 = CodeConsInstruction.new(@context)
+  end
+  
+  it "should have the right context" do
+    @i1.context.should == @context
+  end
+  
+  [:preconditions?, :setup, :derive, :cleanup].each do |methodName|
+    it "should respond to \##{methodName}" do
+      @i1.should respond_to(methodName)
+    end
+  end
+  
+  describe "\#go" do
+    before(:each) do
+      @context = Interpreter.new
+      @i1 = CodeConsInstruction.new(@context)
+    end
+    
+    describe "\#preconditions?" do
+      it "should need at least two items on the :code stack" do
+        @context.clear_stacks
+        lambda{@i1.preconditions?}.should raise_error
+        @context.stacks[:code].push(ValuePoint.new("code", "ref b"))
+        @context.stacks[:code].push(ValuePoint.new("code", "ref a"))
+        lambda{@i1.preconditions?}.should_not raise_error
+      end
+    end
+    
+    describe "\#cleanup" do
+      it "should push a new concatenated block containing the two :code items' code, in order" do
+        @context.stacks[:code].push(ValuePoint.new("code", "ref a"))
+        @context.stacks[:code].push(ValuePoint.new("code", "ref b"))
+        @i1.go
+        @context.stacks[:code].peek.value.should == "block {\n  ref a\n  ref b}"
+        
+        @context.stacks[:code].push(ValuePoint.new("code", "block {ref a}"))
+        @context.stacks[:code].push(ValuePoint.new("code", "ref b"))
+        @i1.go
+        @context.stacks[:code].peek.value.should ==
+          "block {\n  block {\n    ref a}\n  ref b}"
+      end
+      
+      it "should raise an exception if either code item can't be parsed" do
+        @context.stacks[:code].push(ValuePoint.new("code", "somethin' weird"))
+        @context.stacks[:code].push(ValuePoint.new("code", "ref b"))
+        @i1.go
+        @context.stacks[:code].depth.should == 0
+        @context.stacks[:error].peek.value.should == "code_cons cannot parse the arguments"
+      end
+      
+      it "should work with footnotes" do
+        @context.stacks[:code].push(ValuePoint.new("code", "value «int»\n«int» 7771"))
+        @context.stacks[:code].push(ValuePoint.new("code", "value «int»\n«int» 1117"))
+        @i1.go
+        @context.stacks[:code].peek.value.should ==
+          "block {\n  value «int»\n  value «int»} \n«int» 7771\n«int» 1117"
+      end
+    end
+  end
+end
+
+
+
+
 describe CodePositionInstruction do
   before(:each) do
     @context = Interpreter.new
