6d09d757f0b7894d21b858da1f004c1b17239a57
Bill Tozier <bill@tmesis.local>
2010-03-28 14:55:30 -0400

    playing a bit with scanning mutagenesis for a slide

---
+++ _spikes/scanning_mutagenesis.rb
@@ -0,0 +1,26 @@
+#encoding: utf-8
+require '../lib/nudge'
+include Nudge
+
+@wt = NudgeProgram.random(target_size_in_points:20)
+
+def scanning_mutagenesis
+
+  (0..@wt.tidy.length).each do |char|
+    zapped = @wt.blueprint.clone
+    zapped[char] = 'X'
+    mutant = NudgeProgram.new(zapped)
+    puts "#{zapped.inspect},#{mutant.parses?}"
+  end
+end
+
+def scanning_deletion
+  (1..20).each do |pt|
+    zapped = @wt.delete_point(pt)
+    puts "#{zapped.blueprint.inspect},#{zapped.parses?}"
+  end
+end
+
+scanning_mutagenesis
+puts "\n\n"
+scanning_deletion
