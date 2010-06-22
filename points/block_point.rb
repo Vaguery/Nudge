16f5093eb7d9af8aeb3c6fc982e18ddd4d6b596e
Jesse Sielaff <jesse.sielaff@gmail.com>
2010-06-22 15:21:46 -0400

    Made point deletion 1.9 compatible.

--- points/block_point.rb
+++ points/block_point.rb
@@ -36,9 +36,10 @@ class BlockPoint < NudgePoint
         
         case action
           when :get
-          when :replace, :delete  then @points[i..i] = new_point
-          when :insert_before     then @points[i...i] = new_point
-          when :insert_after      then @points[(i + 1)...(i + 1)] = new_point
+          when :delete         then @points.delete_at(i)
+          when :replace        then @points[i..i] = new_point
+          when :insert_before  then @points[i...i] = new_point
+          when :insert_after   then @points[(i + 1)...(i + 1)] = new_point
         end
         
         throw(:complete, old_point)
