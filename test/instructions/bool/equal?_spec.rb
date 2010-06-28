1a51ab84f9182d9957e30a9ec37c7aedd8707a7e
Jesse Sielaff <jesse.sielaff@gmail.com>
2010-06-27 23:00:25 -0400

    Fixed bug in BoolEqualQ spec.

--- test/instructions/bool/equal?_spec.rb
+++ test/instructions/bool/equal?_spec.rb
@@ -3,7 +3,7 @@ require 'nudge'
 describe "BoolEqualQ" do
   describe "#process()" do
     it "pops the top two items off the :bool stack and pushes their logical == onto the :bool stack" do
-      script = "block { value «bool» value «bool» do bool_equal_q }\n«bool» false \n«bool» false"
+      script = "block { value «bool» value «bool» do bool_equal? }\n«bool» false \n«bool» false"
       outcome = Executable.new(script).run
       outcome.stacks[:bool].length.should == 1
       outcome.stacks[:bool][-1].should == "true"
