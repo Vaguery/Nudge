e59aa93c6d970f2651f2d6ed9771ad4e9a93a813
Jesse Sielaff <jesse.sielaff@gmail.com>
2010-07-21 14:36:18 -0400

    Moved code for execution of instructions.

--- nudge_instruction.rb
+++ nudge_instruction.rb
@@ -25,51 +25,42 @@ class NudgeInstruction
   end
   
   def NudgeInstruction.execute (instruction_name, outcome_data)
-    instruction = INSTRUCTIONS[instruction_name].new(outcome_data)
-    stacks = outcome_data.stacks
-    
-    if instruction.get_required_arguments!(stacks)
-      instruction.process
-      instruction.push_results_to_stacks!(stacks)
-    end
-    
-  rescue NudgeError => error
-    stacks[:error].push(error.string)
+    INSTRUCTIONS[instruction_name].new(outcome_data).execute
   end
   
   def initialize (outcome_data)
     @outcome_data = outcome_data
-    @argument_stacks = {}
-    @result_stacks = {}
+    @argument_stacks = Hash.new {|hash, key| hash[key] = [] }
+    @result_stacks = Hash.new {|hash, key| hash[key] = [] }
+  end
+  
+  def execute
+    stacks = @outcome_data.stacks
+    
+    self.class::REQUIREMENTS.each do |value_type, n|
+      return unless (stack = stacks[value_type]) && (stack.length >= n)
+    end.each do |value_type, n|
+      stack = stacks[value_type]
+      argument_stack = @argument_stacks[value_type]
+      
+      n.times do
+        argument = (value_type == :exec) ? stack.pop : stack.pop.send(:"to_#{value_type}")
+        argument_stack << argument
+      end
+    end
+    
+    process
+    
+    @result_stacks.each do |value_type, result_stack|
+      stacks[value_type].concat result_stack
+    end
+    
+  rescue NudgeError => error
+    stacks[:error] << error.string
   end
   
   def put (value_type, result)
-    result_stack = @result_stacks[value_type] ||= []
-    result_stack << (value_type == :exec ? result : result.to_s)
-  end
-  
-  def get_required_arguments! (stacks)
-    self.class::REQUIREMENTS.each do |value_type, n|
-      if !(stack = stacks[value_type]) || (stack.length < n)
-        return false
-      end
-    end.each do |value_type, n|
-      stack = stacks[value_type]
-      argument_stack = @argument_stacks[value_type] ||= []
-      
-      n.times do
-        argument_stack << (value_type == :exec ? stack.pop : stack.pop.send(:"to_#{value_type}"))
-      end
-    end
-  end
-  
-  def push_results_to_stacks! (stacks)
-    @result_stacks.each do |value_type, result_stack|
-      stack = stacks[value_type]
-      
-      result_stack.each do |result|
-        stack.push(result)
-      end
-    end
+    result = (value_type == :exec) ? result : result.to_s
+    @result_stacks[value_type] << result
   end
 end
