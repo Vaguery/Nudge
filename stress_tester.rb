964aecf59bcb7a9a0fda39aaa232f2bf0b08c683
Bill Tozier <bill@vagueinnovation.com>
2010-08-07 21:39:33 -0400

    making and running random programs to stress-test and find edge cases

---
+++ stress_tester.rb
@@ -0,0 +1,24 @@
+# encoding: UTF-8
+require File.expand_path("nudge", File.dirname(__FILE__))
+
+puts "points, steps, time, :bool, :code, :error, :exec, :float, :int, :proportion"
+
+100000.times do
+  begin
+
+    script = NudgeWriter.new.random
+    exe = NudgeExecutable.new(script)
+    exe.set_options(point_limit:20000, time_limit:20)
+
+    exe.bind(:x1 => Value.new(:int, 100), :x2 => Value.new(:bool, false))
+  
+    o = exe.run
+    puts "#{NudgePoint.from(script).points}, #{o.points_evaluated}, #{o.time_elapsed}, #{o.stacks[:bool].length}, #{o.stacks[:code].length}, #{o.stacks[:error].length}, #{o.stacks[:exec].length}, #{o.stacks[:float].length}, #{o.stacks[:int].length}, #{o.stacks[:proportion].length}"
+  
+  rescue=> err
+    puts err
+    puts script
+    puts o.stacks.inspect
+    break
+  end
+end
