3bd77508bce2643b1a68c71c06bdb938958833ed
Bill Tozier <bill@vagueinnovation.com>
2010-08-08 08:28:29 -0400

    watching for silent Infinity results in stress_tester.rb

--- stress_tester.rb
+++ stress_tester.rb
@@ -13,9 +13,13 @@ puts "points, steps, time, :bool, :code, :error, :exec, :float, :int, :proportio
     exe.bind(:x1 => Value.new(:int, 100), :x2 => Value.new(:bool, false))
   
     o = exe.run
+    
+    raise "INFINITY in :int" if o.stacks[:int].any? {|i| i =~ /Infi/}
+    raise "INFINITY in :float" if o.stacks[:float].any? {|i| i =~ /Infi/}
+    
     puts "#{NudgePoint.from(script).points}, #{o.points_evaluated}, #{o.time_elapsed}, #{o.stacks[:bool].length}, #{o.stacks[:code].length}, #{o.stacks[:error].length}, #{o.stacks[:exec].length}, #{o.stacks[:float].length}, #{o.stacks[:int].length}, #{o.stacks[:proportion].length}"
   
-  rescue=> err
+  rescue => err
     puts "----------"
     puts "FAILED after #{exe.points_evaluated} steps with err \"#{err}\""
     puts "----------"
