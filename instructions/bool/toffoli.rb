233eb8fa5776f66b7b9a2b54f015f4a21f54846d
Bill Tozier <bill@vagueinnovation.com>
2010-09-26 11:03:09 -0400

    finished bool/toffoli_spec.rb [stupid rSpec vs 1.9.2 scope errors)

--- instructions/bool/toffoli.rb
+++ instructions/bool/toffoli.rb
@@ -5,6 +5,6 @@ class BoolToffoli < NudgeInstruction
   def process
     put :bool, bool(2)
     put :bool, bool(1)
-    put :bool, bool(0) ^ (bool(2) && bool(1))
+    put :bool, bool(0) != (bool(2) && bool(1))
   end
 end
--- spec/instructions/bool/toffoli_spec.rb
+++ spec/instructions/bool/toffoli_spec.rb
@@ -15,8 +15,40 @@ describe "BoolToffoli" do
   end
   
   describe "#process and #effects" do
-    it "should be easier to spec this :/" do
-      pending
+    before(:each) do
+      @combos = [[true, true, true], [true, true, false], [true, false, true], [true, false, false], [false, true, true], [false, true, false], [false, false, true], [false, false,false]]
     end
+    
+    
+    it "should leave the first input unchanged" do
+      @combos.each do |combo|
+        prog = "block {value «bool» value «bool» value «bool»\n«bool» #{combo[0]}}\n«bool» #{combo[1]}}\n«bool» #{combo[2]}}"
+        @context = NudgeExecutable.new(prog)
+        @context.stacks[:bool] << combo[0] << combo[1] << combo[2]
+        BoolToffoli.new(@context).execute
+        @context.stacks[:bool][-3].should == combo[0].to_s
+      end
+    end
+    
+    it "should leave the second input unchanged" do
+      @combos.each do |combo|
+        prog = "block {value «bool» value «bool» value «bool»\n«bool» #{combo[0]}}\n«bool» #{combo[1]}}\n«bool» #{combo[2]}}"
+        @context = NudgeExecutable.new(prog)
+        @context.stacks[:bool] << combo[0] << combo[1] << combo[2]
+        BoolToffoli.new(@context).execute
+        @context.stacks[:bool][-2].should == combo[1].to_s
+      end
+    end
+    
+    it "should return Toffoli operator on top" do
+      @combos.each do |combo|
+        prog = "block {value «bool» value «bool» value «bool»\n«bool» #{combo[0]}}\n«bool» #{combo[1]}}\n«bool» #{combo[2]}}"
+        @context = NudgeExecutable.new(prog)
+        @context.stacks[:bool] << combo[0] << combo[1] << combo[2]
+        BoolToffoli.new(@context).execute
+        @context.stacks[:bool][-1].should == (combo[2] != (combo[0] && combo[1])).to_s 
+      end
+    end
+    
   end
 end
